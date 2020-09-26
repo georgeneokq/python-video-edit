a9053e2e227f3deac84f1773c931932a11c3dce9
George <george.neokq@gmail.com>
2020-09-26 18:39:54 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Python-Video-Edit
+
+## What's this?
+A python program I created to synchronize audio of video files.
+
+I was watching a video and the audio was obviously lacking behind. I wanted to find a simple console program to adjust the audio as I didn't want to download some huge software just to complete the task. I couldn't find one that suited my needs (or maybe I just didn't search hard enough) so I decided to take matters into my own hands.
+
+This program has only been tested for Windows.
+
+## Usage
+python main.py [--seek] SEEK [--video-path] VIDEO-PATH
+
+Where SEEK is a number in seconds and VIDEO-PATH is the full path to the video.
+
+SEEK is a required parameter.
+
+VIDEO-PATH is not required but currently recommended (see "Issues" section below). If VIDEO-PATH is not provided, a file selection dialog will be shown.
+
+Example usage:
+``` python main.py --seek 10 --video-path C:\Users\USERNAME\my_video.mp4 ```
+
+The above command will take the specified video and adjust its audio to start right away from the original 10 second mark. The video will be saved into this project's "output" folder, but the destination folder can be changed by changing the value of "output_dir" in the config.json file.
+
+## Dependencies
+- tk
+- moviepy
+- numpy
+- scipy
+- ffmpeg (binary provided in "bin" folder)
+
+To install all dependencies using pip:
+
+```pip install -U tk moviepy numpy scipy```
+
+
+## Issues
+After running this program a few times (roughly 5 times) tkinter's ```askopenfiledialog``` function will cease to work.
+
+It will then start to freeze other running processes. A computer restart fixes the problem, but there are cases where the issue fixes itself over time. It is currently recommended to directly provide the video file path to the program using the --video-path option on the command line to avoid the usage of the file dialog.
---
+++ media_editor.py
@@ -0,0 +1,74 @@
+'''
+USEFUL INFO:
+
+There are 44100 frames per second in an audio clip by default.
+The default sample rate is 16-bit (2 bytes).
+For stereo sound, the size would increase to 4 bytes per frame.
+'''
+
+import subprocess
+import os.path as path
+import os
+import json
+from moviepy.editor import *
+import json
+import numpy as np
+from scipy.io.wavfile import write as wav_write
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+ffmpeg_path = path.join(os.getcwd(), config['ffmpeg_path'])
+print('[INFO] Using ffmpeg binary from: {}'.format(ffmpeg_path))
+
+# t is either negative or positive. If positive, the audio will start earlier
+# If negative, audio will start later
+def seek_audio(src_audio_path, seek_time):
+    new_file_path = src_audio_path + '.seek_{}_seconds.wav'.format(seek_time)
+    audioclip = AudioFileClip(src_audio_path)
+    sound_array = audioclip.to_soundarray()
+    sound_array = np.array(sound_array)
+    sample_rate = audioclip.fps # default is 44100 times per second
+    duration = audioclip.duration
+
+    time = abs(seek_time)
+    empty_sound_count = int(time * sample_rate)
+    empty_sound = [0, 0]
+    empty_sounds = np.zeros((empty_sound_count, 2))
+    
+    # If seek_time > 0, put the empty sound at the back after shifting sound to front
+    # If seek time < 0, put the empty sound at the front after shifting sound to back (sound at back is lost)
+    if seek_time > 0:
+        sound_array = sound_array[empty_sound_count:len(sound_array)]
+        sound_array = np.append(sound_array, empty_sounds)
+    else:
+        sound_array = sound_array[0:len(sound_array) - empty_sound_count]
+        sound_array = np.append(empty_sounds, sound_array)
+
+    sound_array = np.reshape(sound_array, (len(sound_array) // audioclip.nchannels, audioclip.nchannels))
+
+    scaled = np.int16(sound_array/np.max(np.abs(sound_array)) * 32767)
+    os.remove(src_audio_path)
+    wav_write(new_file_path, sample_rate, scaled)
+    
+    return new_file_path
+
+
+def sep_audio_video(src_file_path, output_folder='output'):
+    file_count = len(os.listdir(output_folder)) # File count in output folder
+    new_file_name = '{}.wav'.format(file_count + 1)
+    new_file_path = path.join(output_folder, new_file_name)
+    command = '{} -i {} -ab 160k -ac 2 -ar 44100 -vn {}'.format(ffmpeg_path, src_file_path, new_file_path)
+    subprocess.call(command, shell=True)
+    return new_file_path
+
+
+def combine_audio_video(src_video_path, src_audio_path, output_folder='output'):
+    file_count = len(os.listdir(output_folder)) # File count in output folder
+    new_video_file_name = '{}.mp4'.format(file_count)
+    new_video_file_path = path.join(output_folder, new_video_file_name)
+    src_videoclip = VideoFileClip(src_video_path)
+    src_audioclip = AudioFileClip(src_audio_path)
+    new_videoclip = src_videoclip.set_audio(src_audioclip)
+    new_videoclip.write_videofile(new_video_file_path)
+    os.remove(src_audio_path)
