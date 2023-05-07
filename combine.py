4a8cae02ff6b787026952b2a56f3f3bf687ce003
George <george.neokq@gmail.com>
2023-05-07 19:19:08 +0800

    add requirements.txt and combine.py

---
+++ combine.py
@@ -0,0 +1,7 @@
+from media_editor import combine_audio_video
+import sys
+
+if __name__ == '__main__':
+  video_file_path = sys.argv[1]
+  audio_file_path = sys.argv[2]
+  combine_audio_video(video_file_path, audio_file_path, output_folder='.')
