dcf431304f6ce59f8c5d8c673bcf80b4850ab20c
George <george.neokq@gmail.com>
2023-09-29 21:18:50 +0800

    fix combine.py extra argument

--- combine.py
+++ combine.py
@@ -4,4 +4,4 @@ import sys
 if __name__ == '__main__':
     video_file_path = sys.argv[1]
     audio_file_path = sys.argv[2]
-    combine_audio_video(video_file_path, audio_file_path, output_folder='.')
+    combine_audio_video(video_file_path, audio_file_path)
