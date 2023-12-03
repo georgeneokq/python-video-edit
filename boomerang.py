4354a4ccb7d8d4e912ab098db47d5eff8b52ef5b
George <george.neokq@gmail.com>
2023-12-03 10:19:11 +0800

    allow usage of full duration in boomerang effect

--- boomerang.py
+++ boomerang.py
@@ -13,14 +13,13 @@ from gif import convert_to_gif
 reverse_script = 'reverse.py'
 concatenate_script = 'concatenate.py'
 gif_script = 'gif.py'
-DEFAULT_DURATION = 2
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input_files', type=str, nargs='+')
     parser.add_argument('-k', '--keep', action='store_true', help='Keep files used to generate the final GIF')
-    parser.add_argument('-d', '--duration', type=float, help='Total duration of boomerang, default 2s')
+    parser.add_argument('-d', '--duration', type=float, default=2, help='Total duration of boomerang, default 2s')
     args = parser.parse_args()
 
     input_files = args.input_files
@@ -41,30 +40,35 @@ if __name__ == '__main__':
         reversed_video_path = reverse_video(input_file)
 
         # Concatenate
-        concatenated_video_path = f'{input_file_basename}-concat.mp4'
-        concatenate([input_file, reversed_video_path], concatenated_video_path)
+        concatenated_clip_path = os.path.join(parent_folders, f'{input_file_basename}-concat.mp4')
+        concatenate([input_file, reversed_video_path], concatenated_clip_path)
 
-        clip = VideoFileClip(concatenated_video_path)
+        concatenated_clip = VideoFileClip(concatenated_clip_path)
 
-        # Shorten to specified number of seconds, by default 2.
-        final_duration = duration if duration is not None else DEFAULT_DURATION
+        # If duration is specified to be 0, instead use the
+        # full length of the video without shortening it.
+        if duration != 0:
+            clip_path = f'{input_file_basename}-concat-shortened.mp4'
+            shortened_clip: VideoFileClip = concatenated_clip.fx(speedx, final_duration=duration)
 
-        shortened_clip_path = f'{input_file_basename}-concat-shortened.mp4'
-        shortened_clip: VideoFileClip = clip.fx(speedx, final_duration=final_duration)
+            print(f'\nGenerating boomerang with duration of {duration} seconds\n')
 
-        print(f'\nGenerating boomerang with duration of {final_duration} seconds\n')
+            # Write the shortened clip to file
+            shortened_clip.write_videofile(clip_path)
+            shortened_clip.close()
+        else:
+            clip_path = concatenated_clip_path
 
-        # Write the shortened clip to file
-        shortened_clip.write_videofile(shortened_clip_path)
-
-        # Cleanup
-        clip.close()
-        shortened_clip.close()
+        concatenated_clip.close()
 
         # Convert to gif
-        convert_to_gif(shortened_clip_path, output_file_path)
+        convert_to_gif(clip_path, output_file_path)
 
         if not keep:
             os.remove(reversed_video_path)
-            os.remove(concatenated_video_path)
-            os.remove(shortened_clip_path)
+            os.remove(concatenated_clip_path)
+
+            # If not specified to use the full length of the video,
+            # there will be an additional shortened video file
+            if duration != 0:
+                os.remove(clip_path)
